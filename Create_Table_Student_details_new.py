2afa7bbf6531da39049c29cfc928f338ba68c6a8
Joliz Anton <jolizanton.v.j@gmail.com>
2020-08-07 21:48:24 +0530

    main

--- Create_Table_Student_details_new.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('student_details.db')
-cursor = conn.cursor()
-# create a table
-conn.execute('''CREATE TABLE STUDENT_DETAILS_NEW
-         (ROLL_NUMBER INT PRIMARY KEY     NOT NULL,
-         NAME           TEXT    NOT NULL,
-         CLASS            INT     NOT NULL);''')
